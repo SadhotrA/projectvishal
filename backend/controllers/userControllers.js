bdc49b798e8ba9d957777673f8e3faa54fb71a46
Vishal Sadhotra <118679116+SadhotrA@users.noreply.github.com>
2024-10-04 14:37:07 +0530

    ulpoading a new project

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,109 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../config/generateToken");
+
+//@description     Get or Search all users
+//@route           GET /api/user?search=
+//@access          Public
+const mongoose = require('mongoose');
+
+const allUsers = asyncHandler(async (req, res) => {
+  try {
+    // Log the current user ID for debugging
+    console.log("Current user ID:", req.user._id);
+
+    const keyword = req.query.search
+      ? {
+          $or: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i" } },
+          ],
+        }
+      : {};
+
+    // Combine the keyword with the condition to exclude the current user
+    const searchCriteria = {
+      ...keyword,
+      _id: { $ne: mongoose.Types.ObjectId(req.user._id) }
+    };
+
+    // Log the search criteria for debugging
+    console.log("Search criteria:", searchCriteria);
+
+    const users = await User.find(searchCriteria);
+
+    // Log the found users for debugging
+    console.log("Found users:", users);
+
+    res.send(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).send({ message: "Error fetching users" });
+  }
+});
+
+
+//@description     Register new user
+//@route           POST /api/user/
+//@access          Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+
+//@description     Auth the user
+//@route           POST /api/users/login
+//@access          Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { allUsers, registerUser, authUser };
